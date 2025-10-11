a9c727f84d89f88bf6b61342d15c160c51c98799
alialhassan12 <alialhasan65t@gmail.com>
2025-10-11 10:51:42 +0300

    signup frontend implemented

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import cors from 'cors'
 import { connectDB } from './lib/db.js';
 
 //import routes
@@ -16,6 +17,7 @@ async function startServer(){
 }
 
 app.use(express.json());
+app.use(cors({origin:process.env.CLIENT_URL,credentials:true}));
 app.use(cookieParser());
 
 app.use('/api/auth',authRoute);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,41 @@
-import { Route, Routes } from "react-router";
-
+import { Navigate, Route, Routes } from "react-router";
+import { Toaster } from "react-hot-toast";
 // import pages
 import ChatPage from "./pages/ChatPage";
 import SignupPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 
+//import hooks
+import { useAuthStore } from "./store/useAuthStore.js";
+import { useEffect } from "react";
+
+//import components
+import Loading from './components/Loading';
+
 function App() {
+  const {authUser,isCheckingAuth,checkAuth}=useAuthStore();
+  
+  useEffect(()=>{
+    checkAuth();
+    console.log(authUser); 
+  },[checkAuth]);
+
+  if(isCheckingAuth) return <Loading></Loading>
+
   return (
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden ">
       {/* DECORATORS - GRID BG & GLOW SHAPES */}
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
+
       <Routes>
-        <Route path="/" element={<ChatPage/>}></Route>
-        <Route path="/signup" element={<SignupPage/>}></Route>
-        <Route path="/login" element={<LoginPage/>}></Route>
+        <Route path="/" element={authUser?<ChatPage/>:<Navigate to={"/login"}/>}></Route>
+        <Route path="/signup" element={!authUser?<SignupPage/>:<Navigate to={"/"}/>}></Route>
+        <Route path="/login" element={!authUser?<LoginPage/>:<Navigate to={"/"}/>}></Route>
       </Routes>
+      
+    <Toaster/>
     </div>
   )
 }
