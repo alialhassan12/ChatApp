c3886efceb718792d9daba731921d6f200bbeef4
alialhassan12 <alialhasan65t@gmail.com>
2025-10-14 12:44:57 +0300

    fix default route newer version

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,8 @@ app.use('/api/message',messageRoute);
 if (process.env.NODE_ENV=== "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get('/:path(*)', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+    app.get('/.*/', (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
 }
 
