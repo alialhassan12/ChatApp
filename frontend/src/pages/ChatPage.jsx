8922870fa5acf13ed2664e89be2d58b433d152f5
alialhassan12 <alialhasan65t@gmail.com>
2025-10-14 13:56:46 +0300

    responsive design

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -13,20 +13,22 @@ export default function ChatPage(){
 
     return(
         <div className="relative w-full max-w-6xl h-[800px] ">
-            <BorderAnimatedContainer  >
-                {/* LEFT SIDE */}
-                <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
-                    <ProfileHeader />
-                    <ActiveTabSwitch />
+            <BorderAnimatedContainer >
+                <div className="flex h-full w-full max-[840px]:flex-col  lg:flex-row chatSmallScreen">
+                    {/* LEFT SIDE */}
+                    <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col max-[840px]:w-full lg:w-80">
+                        <ProfileHeader />
+                        <ActiveTabSwitch />
 
-                    <div className="flex-1 overflow-y-auto p-4 space-y-2">
-                        {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+                        <div className="flex-1 overflow-y-auto p-4 space-y-2">
+                            {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+                        </div>
                     </div>
-                </div>
 
-                {/* RIGHT SIDE */}
-                <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
-                    {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
+                    {/* RIGHT SIDE */}
+                    <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm  h-full">
+                        {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
+                    </div>
                 </div>
             </BorderAnimatedContainer>
         </div>
